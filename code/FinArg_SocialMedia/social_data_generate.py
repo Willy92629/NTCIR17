dcbf348d1b382cfa1f8eb19d4472bcb49c0b7904
Leonardo Lin <67550587+leonardo-lin@users.noreply.github.com>
2023-05-06 11:37:12 +0800

    deberta baseline

---
+++ code/FinArg_SocialMedia/social_data_generate.py
@@ -0,0 +1,30 @@
+print("hi")
+import json
+import random
+def gen_data(filepath,outpath):
+    output=[]
+    zero_data=[]
+    with open(filepath,'r',encoding='utf8') as f:
+        datas=json.load(f)
+        for i in datas:
+            #print(i)
+            if i[2]==2:
+                i[2]=1
+            output.append(i)
+            if i[2]==0:
+                zero_data.append(i)
+        output=output+zero_data+zero_data+zero_data+zero_data+zero_data
+        
+        random.shuffle(output)
+        print(len(output))
+        output_1=output[0:9500]
+        output_2=output[9500:9939]
+    with open(outpath,'w',encoding='utf-8') as f2:
+        json.dump(output,f2,ensure_ascii=False)
+    #with open("test_social_2classification.json",'w',encoding='utf-8') as f2:
+    #    json.dump(output_2,f2,ensure_ascii=False)
+
+
+gen_data("train_social.json","train_social_2classification.json")
+#gen_data("dev_social.json","dev_social_2classification.json")
+
