a31cc9c8181dfd2451e3f4173e34f016c70e6628
leonardo-lin <st950065@yahoo.com.tw>
2023-05-06 23:45:43 +0800

    gen_att_sup

--- code/FinArg_SocialMedia/social_data_generate.py
+++ code/FinArg_SocialMedia/social_data_generate.py
@@ -25,6 +25,55 @@ def gen_data(filepath,outpath):
     #    json.dump(output_2,f2,ensure_ascii=False)
 
 
-gen_data("./../../../data/FinArg_SocialMedia/train_social.json","./../../../data/FinArg_SocialMedia/train_social_2classification2.json")
-#gen_data("dev_social.json","dev_social_2classification.json")
+#gen_data("./../../../data/FinArg_SocialMedia/train_social.json","./../../../data/FinArg_SocialMedia/train_social_2classification2.json")
+def gen_data2(filepath,outpath,outpath2):
+    output=[]
+    zero_data=[]
+    with open(filepath,'r',encoding='utf-8') as f:
+        datas=json.load(f)
+        for i in datas:
+            #print(i)
+            if i[2]==0:
+                continue
+            if i[2]==2:
+                i[2]=0
+            output.append(i)
+            
+        #output=output+zero_data+zero_data+zero_data+zero_data+zero_data
+        
+        random.shuffle(output)
+        print(len(output)) 
+        output_1=output[0:5334]
+        output_2=output[5334:5834]
+    with open(outpath,'w',encoding='utf-8') as f2:
+        json.dump(output_1,f2,ensure_ascii=False)
+    with open(outpath2,'w',encoding='utf-8') as f2:
+        json.dump(output_2,f2,ensure_ascii=False)
+
+def gen_data3(filepath,outpath):
+    output=[]
+    zero_data=[]
+    with open(filepath,'r',encoding='utf-8') as f:
+        datas=json.load(f)
+        for i in datas:
+            #print(i)
+            if i[2]==0:
+                continue
+            if i[2]==2:
+                i[2]=0
+            output.append(i)
+            
+        #output=output+zero_data+zero_data+zero_data+zero_data+zero_data
+        
+        random.shuffle(output)
+        print(len(output)) 
+        #output_1=output[0:5334]
+        #output_2=output[5334:5834]
+    with open(outpath,'w',encoding='utf-8') as f2:
+        json.dump(output,f2,ensure_ascii=False)
+    #with open(outpath2,'w',encoding='utf-8') as f2:
+    #    json.dump(output_2,f2,ensure_ascii=False)
+
+#gen_data2("./../../../data/FinArg_SocialMedia/train_social.json","./../../../data/FinArg_SocialMedia/train_social_att_sup.json","./../../../data/FinArg_SocialMedia/test_social_att_sup.json")
+gen_data3("./../../../data/FinArg_SocialMedia/dev_social.json","./../../../data/FinArg_SocialMedia/dev_social_2classification_att_sup.json")
 
