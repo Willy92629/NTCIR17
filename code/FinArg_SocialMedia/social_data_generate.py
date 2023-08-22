bc2989edc670494dcb0ad8047fa13895300ba2aa
leonardo-lin <st950065@yahoo.com.tw>
2023-08-22 22:55:14 +0800

    delete no use file
    delete no use

--- code/FinArg_SocialMedia/social_data_generate.py
+++ code/FinArg_SocialMedia/social_data_generate.py
@@ -1,6 +1,7 @@
 print("hi")
 import json
 import random
+#生成0和非0
 def gen_relation_2classification(filepath,outpath,outpath2):
     output=[]
     zero_data=[]
@@ -17,32 +18,32 @@ def gen_relation_2classification(filepath,outpath,outpath2):
                 zero_data.append(i)
         random.shuffle(zero_data)
         random.shuffle(output)
-        z_data=zero_data[0:600]
-        ztest_data=zero_data[600:684]
+        z_data=zero_data
+        #ztest_data=zero_data[600:684]
         print(len(output))
-        output1=output[0:5734]
-        output_test=output[5734:5834]
+        output1=output
+        #output_test=output[5734:5834]
         print(len(output1))
         print(len(z_data))
-        output1=output1+z_data+z_data+z_data+z_data+z_data+z_data+z_data
-        
-        print(len(output_test))
-        print(len(ztest_data))
-        output_test=output_test+ztest_data
+        #output1=output1+z_data+z_data+z_data+z_data+z_data+z_data+z_data
+        output1=output1+z_data+z_data+z_data+z_data+z_data
+        #print(len(output_test))
+        #print(len(ztest_data))
+        #output_test=output_test+ztest_data
         random.shuffle(output1)
-        random.shuffle(output_test)
+        #random.shuffle(output_test)
         print(len(output1)) 
-        print(len(output_test))
+        #print(len(output_test))
         output_1=output1
-        output_2=output_test
+        #output_2=output_test
     with open(outpath,'w',encoding='utf-8') as f2:
         json.dump(output_1,f2,ensure_ascii=False)
-    with open(outpath2,'w',encoding='utf-8') as f2:
-        json.dump(output_2,f2,ensure_ascii=False)
+    #with open(outpath2,'w',encoding='utf-8') as f2:
+    #    json.dump(output_2,f2,ensure_ascii=False)
 
 
 
-#gen_relation_2classification("./../../../data/FinArg_SocialMedia/train_social.json","./../../../data/FinArg_SocialMedia/train_social_relation.json","./../../../data/FinArg_SocialMedia/test_social_relation.json")
+gen_relation_2classification("./../../../data/FinArg_SocialMedia/dev_social2.json","./../../../data/FinArg_SocialMedia/dev_social_relation.json","./../../../data/FinArg_SocialMedia/test_social_relation.json")
 def gen_att_support_data(filepath,outpath,outpath2):
     output=[]
     zero_data=[]
@@ -60,12 +61,16 @@ def gen_att_support_data(filepath,outpath,outpath2):
         
         random.shuffle(output)
         print(len(output)) 
+        print("hey")
         output_1=output[0:5334]
         output_2=output[5334:5834]
     with open(outpath,'w',encoding='utf-8') as f2:
+        json.dump(output,f2,ensure_ascii=False)
+    """with open(outpath,'w',encoding='utf-8') as f2:
         json.dump(output_1,f2,ensure_ascii=False)
     with open(outpath2,'w',encoding='utf-8') as f2:
-        json.dump(output_2,f2,ensure_ascii=False)
+        json.dump(output_2,f2,ensure_ascii=False)"""
+#gen_att_support_data("./../../../data/FinArg_SocialMedia/dev_social2.json","./../../../data/FinArg_SocialMedia/dev_social_att_sup.json","./../../../data/FinArg_SocialMedia/test_social_att_sup.json")
 
 def gen_dev_att_support_data(filepath,outpath):
     output=[]
@@ -133,4 +138,4 @@ def gen_final_3classification_data(filepath,filepath2,outpath):
         json.dump(output,f2,ensure_ascii=False)
     #with open(outpath2,'w',encoding='utf-8') as f2:
     #    json.dump(output_2,f2,ensure_ascii=False)
-gen_final_3classification_data("./../../../data/FinArg_SocialMedia/test_social_att_sup.json","./../../../data/FinArg_SocialMedia/test_social_relation.json","./../../../data/FinArg_SocialMedia/test2_social.json")
+#gen_final_3classification_data("./../../../data/FinArg_SocialMedia/test_social_att_sup.json","./../../../data/FinArg_SocialMedia/test_social_relation.json","./../../../data/FinArg_SocialMedia/test2_social.json")
